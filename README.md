792ab08dbb595555e87feae6af805aaba17350a5
lumilitas <lucasmengineer@edu.unifor.br>
2021-11-06 08:00:46 -0300

    Finished challenge

---
+++ README.md
@@ -0,0 +1,10 @@
+# TODO Challenge
+
+Essa será uma aplicação para gerenciar tarefas (em inglês todos). 
+
+- [x] Será permitida a criação de um usuário com name e username
+- [x] Criar um novo todo
+- [x] Listar todos os todos
+- [x] Alterar o title e deadline de um todo existente
+- [x] Marcar um todo como feito
+- [x] Excluir um todo
--- src/index.js
+++ src/index.js
@@ -1,41 +1,118 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User don't exist!" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const user = users.find(user => user.username === username);
+
+  if (user) {
+    return response.status(400).json({ error: "Username already exist! "});
+  }
+
+  const newUser = { 
+    id: uuidv4(),
+    name, 
+    username, 
+    todos: []
+  };
+
+  users.push(newUser);
+
+  return response.json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = { 
+    id: uuidv4(), // precisa ser um uuid
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+
+  if (todoIndex < 0) {
+    return response.status(404).json({ error: "Todo don't exist! "});
+  }
+
+  user.todos[todoIndex].title = title;
+  user.todos[todoIndex].deadline = deadline;
+
+  return response.json(user.todos[todoIndex]);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+  
+  if (todoIndex < 0) {
+    return response.status(404).json({ error: "todo don't exist!" });
+  }
+    
+  user.todos[todoIndex].done = true;
+
+  return response.json(user.todos[todoIndex]);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (todoIndex < 0) {
+    return response.status(404).json({ error: "todo don't exist!" });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
